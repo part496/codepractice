29ee7c44399305fd8cae9caabf266156ff143127
part496 <1037390348@qq.com>
2026-05-27 14:32:54 +0800

    Update Leecode 1 delete repeat.c

--- C/Array/Leecode 1 delete repeat.c
+++ C/Array/Leecode 1 delete repeat.c
@@ -10,7 +10,7 @@ int removeDuplicates(int* nums, int numsSize) {
     return slow;
 }
 //题目链接：https://leetcode.cn/problems/remove-duplicates-from-sorted-array/solutions/728105/shan-chu-pai-xu-shu-zu-zhong-de-zhong-fu-tudo/
-//思路：利用快指针和慢指针 快指针表示遍历数组到达的下标位置，慢指针表示下一个不同元素要填入的下标位置，当快指针发现前后数组值不同时就是，就将快指针所指赋给慢指针，从而达到保持原有相对顺序+去重的效果
+//思路：利用快指针和慢指针 快指针表示遍历数组到达的下标位置，慢指针表示下一个不同元素要填入的下标位置，当快指针发现前后数组值不同时，就将快指针所指赋给慢指针，从而达到保持原有相对顺序+去重的效果
 //Easy
 //时间复杂度o(n)
 //空间复杂度O(1)
