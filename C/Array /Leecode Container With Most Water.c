f0cff765985ed408917cd7e6f2a0c4814d6e9d40
part496 <1037390348@qq.com>
2026-05-30 17:23:32 +0800

    Update Leecode Container With Most Water.c

--- C/Array /Leecode Container With Most Water.c
+++ C/Array /Leecode Container With Most Water.c
@@ -1,24 +1,19 @@
-int maxArea(int* height, int heightSize) {
-    int area = 0;
-    int i = 0, j = heightSize - 1;
-    while(i != j){
-        int minheight;
-        minheight = height[i] > height[j] ? height[j] : height[i];
-        if(height[i] > height[j]){
-           if(area < (j - i) * minheight) area = (j - i) * minheight;
-           j--;
-        }
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if(digits[i] == 9)
+            digits[i] = 0;
         else{
-            if(area < (j - i) * minheight) area = (j - i) * minheight;
-            i++;
-        }
-        
-    }
-    return area;
+            digits[i]++;
+            *returnSize = digitsSize;
+            return digits;
+       }
+   }
+    *returnSize = digitsSize + 1;
+    int *ret = (int*)malloc(sizeof(int)*(digitsSize + 1));
+    memset(ret,0,sizeof(int)*(digitsSize + 1));//将无定义处全部赋0
+    ret[0] = 1;
+    return ret;
 }
-//题目链接：https://leetcode.cn/problems/container-with-most-water/?envType=problem-list-v2&envId=array
-//双指针：创造两个指针从两边向中间靠拢 直到指针相遇
-//由于面积等于底乘相对矮的那个高 所以使相对矮的那个指针向中间移动 这样可以保证所有大的情况都被考虑到
-//虽然底变短了 但高可能变大 面积可能变大
-//反之如果移动那个相对高的那个指针 那么面积一定毕原来的面积小 因为实质上高没变 而底变短了
-//medium
+//题目链接：https://leetcode.cn/problems/plus-one/?envType=problem-list-v2&envId=array
+//只有所有元素全是9的时候才需要扩展数组大小 其他时候正常加一进位即可
+//easy
